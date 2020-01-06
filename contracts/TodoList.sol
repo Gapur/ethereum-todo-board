9356c406faa5ada58d387b878af557cd08867992
Gapur Kassym <gapur.kassym@gmail.com>
2020-01-06 20:32:36 +0600

    feat(TodoList): implement contract and test

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,40 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Welcome Ethereum-Todo-Board");
+  }
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
 }
